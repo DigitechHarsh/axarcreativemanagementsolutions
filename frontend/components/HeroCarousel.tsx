d1a9b5aa62033d6d5ae6b439a1f2794d801c13b0
Harsh Patel <digitech.harsh003@gmail.com>
2026-09-02 13:18:27 +0530

    Change carousel rotation time to 4 seconds

--- frontend/components/HeroCarousel.tsx
+++ frontend/components/HeroCarousel.tsx
@@ -39,7 +39,7 @@ export default function HeroCarousel() {
   useEffect(() => {
     const timer = setInterval(() => {
       setCurrent((prev) => (prev === carouselSlides.length - 1 ? 0 : prev + 1));
-    }, 5000);
+    }, 4000);
     return () => clearInterval(timer);
   }, []);
 
